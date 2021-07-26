b7301d11d49f3c86d6a07b41e82ce45aa0da246a
aflaim7 <aflaim@wustl.edu>
2021-07-26 15:21:04 -0500

    Observed SST coral PSM and modeled vs. observed SST comparisons
    I haven't been able to find observational SSS gridded products that go back to 1850... in fact there might not even be any that go back to 1880. I ran the coral psm using observed SST and modeled SSS just for comparison. The modeled and observed SST are quite different, and therefore the resulting pseudo-proxies are quite different.

--- Iso2k_PRYSM_runs/iso_pseudocoral_plots.R
+++ Iso2k_PRYSM_runs/iso_pseudocoral_plots.R
@@ -1,7 +1,17 @@
+library(geoChronR)
+
+# Import iCESM and observational data
+setwd("~/GitHub/iso-project/Iso2k_PRYSM_runs/data")
+sst = read.csv("ssta_binned.csv", head = T)
+sss = read.csv("sssa_binned.csv", head = T)
+sst_obs = read.csv("binned_BermudaSST.csv", head = T)
+
 # Import PRYSM pseudocoral result
 setwd("~/GitHub/iso-project/Iso2k_PRYSM_runs/results")
 coral = read.csv("pseudocoral.csv", head = F)
 coral = as.vector(unlist(coral))
+coral_obs = read.csv("pseudocoral_obs.csv", head = F)
+coral_obs = as.vector(unlist(coral_obs))
 
 # Time dimension
 start = 1851
@@ -10,6 +20,9 @@ time = seq(from = start, to = end, by = 1)
 
 # Plot pseudocoral TS
 plot(time, coral, type = "l", frame.plot = F, ylab = "pseudo-coral")
+
+# Convert model temps
+sst_c = sst$y - 273.15
 
 #### Load and extract Iso2k ####
 setwd("C:/Users/andre/Box Sync/Konecky Lab/User Storage/Andrew Flaim/Rcode/Iso2k") # Laptop
@@ -40,6 +53,21 @@ plot(binned_proxy$x, binned_proxy$y, type = "l", frame.plot = F,
      ylab = bermuda$paleoData_units, xlab = bermuda$yearUnits,
      main = bermuda$geo_siteName)
 
+#### Plot modeled SST and SSS data ####
+par(mfrow = c(2, 1), omi = c(0.7, 0.4, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
+plot(sst$x, sst_c, type = "l", frame.plot = F, xaxt = "n", xlab = "", lwd = 2,
+     ylab = "Temp (C)", xpd = NA, main = "iCESM Full Forcing Lat:32.5 Lon:-64.7")
+plot(sss$x, sss$y, type = "l", frame.plot = F, xlab = "Year (CE)",
+     ylab = "Salinity (PSU)", xpd = NA, lwd = 2)
+
+#### Plot modeled and observed SST ####
+par(mfrow = c(2, 1), omi = c(0.7, 0.4, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
+plot(sst$x, sst_c, type = "l", frame.plot = F, xaxt = "n", xlab = "", lwd = 2,
+     ylab = "iCESM Temp (C)", xpd = NA, ylim = c(22, 24),
+     main = "Modeled vs. Observed Temp Lat:32.5 Lon:-64.7")
+plot(sst_obs$x, sst_obs$y, type = "l", frame.plot = F, xlab = "Year (CE)",
+     ylab = "COBE-SST2 Temp (C)", xpd = NA, lwd = 2, ylim = c(22, 24))
+
 #### Plot pseudo and binned proxy ####
 par(mfrow = c(2, 1), omi = c(0.7, 0.4, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
 # Pseudo
@@ -56,3 +84,22 @@ cor = cor.test(coral, binned_proxy$y)
 plot(coral, binned_proxy$y, pch = 16, xlab = "Pseudo-coral",
      ylab = "Proxy Data", main = c("r = 0.033", "p = 0.71"))
 
+#### Plot modeled and obs psuedo ####
+par(mfrow = c(3, 1), omi = c(0.7, 0.4, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
+# modeled
+plot(time, coral, type = "l", frame.plot = F, ylab = "Pseudo-coral (modeled)",
+     xaxt = "n", xlab = "", xpd = NA, lwd = 2,
+     main = "Modeled vs. Observed SST Pseudo-corals")
+# observed
+plot(time, coral_obs, type = "l", frame.plot = F, xaxt = "n",
+     ylab = "Pseudo-coral (obs)", xlab = "", xpd = NA,
+     lwd = 2)
+# Proxy
+plot(binned_proxy$x, binned_proxy$y, type = "l", frame.plot = F,
+     ylab = "Proxy data (permil)", xlab = "Year (CE)", xpd = NA,
+     lwd = 2)
+
+#### Plot modeled vs observed correlations ####
+cor = cor.test(coral, coral_obs)
+plot(coral, coral_obs, pch = 16, xlab = "Modeled SST pseudo-coral",
+     ylab = "Observed SST pseudo-coral", main = c("r = 0.133", "p = 0.13"))
