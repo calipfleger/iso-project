8db66e8e10a6f162ac161506fdb98ff45df7bdd7
aflaim7 <aflaim@wustl.edu>
2021-08-20 15:37:27 -0500

    Temperature plot fixes

--- Iso2k_PRYSM_runs/iso_pseudocoral_plots.R
+++ Iso2k_PRYSM_runs/iso_pseudocoral_plots.R
@@ -1,14 +1,14 @@
 library(geoChronR)
 
 # Import iCESM and observational data
-setwd("~/GitHub/iso-project/Iso2k_PRYSM_runs/data/SWBB/SWBB_afedits")
-sst = read.csv("ssta_SWBB1_binned.csv", head = T)
-sss = read.csv("sssa_SWBB1_binned.csv", head = T)
+setwd("~/GitHub/iso-project/Iso2k_PRYSM_runs/data/KUBE/KUBE_afedits")
+sst = read.csv("ssta_KUBE2_binned.csv", head = T)
+sss = read.csv("sssa_KUBE2_binned.csv", head = T)
 #sst_obs = read.csv("binned_BermudaSST.csv", head = T)
 
 # Import PRYSM pseudocoral result
 setwd("~/GitHub/iso-project/Iso2k_PRYSM_runs/results")
-coral = read.csv("SWBB1_pseudocoral.csv", head = F)
+coral = read.csv("KUBE2_pseudocoral.csv", head = F)
 coral = as.vector(unlist(coral))
 #coral_obs = read.csv("pseudocoral_obs.csv", head = F)
 #coral_obs = as.vector(unlist(coral_obs))
@@ -31,7 +31,7 @@ load("iso2k1_0_0.RData")
 rm(D, TS)
 # Apply geoChronR function to extract primary TS
 bermuda = sTS[which(pullTsVariable(sTS, 
-                                      variable = "dataSetName") == "CO96SWBB")]
+                                      variable = "dataSetName") == "CO05KUBE")]
 bermuda = bermuda[[1]] # This is the d18O subset
 
 # Plot raw proxy data
@@ -53,11 +53,37 @@ plot(binned_proxy$x, binned_proxy$y, type = "l", frame.plot = F,
      ylab = bermuda$paleoData_units, xlab = bermuda$yearUnits,
      main = bermuda$geo_siteName)
 
+#### Full data stack ####
+par(mfrow = c(4, 1), omi = c(0.5, 0.3, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
+# SST
+plot(sst$x, sst_c, type = "l", frame.plot = F, xaxt = "n", xlab = "", lwd = 2,
+     ylab = "CESM SST (C)", xpd = NA, main = "KUBE2 Lat:32.467, Lon:-64.7", 
+     cex.lab = 1.5, cex.axis = 1.5)
+# SSS
+plot(sss$x, sss$y, type = "l", frame.plot = F, xlab = "", xaxt = "n",
+     ylab = "CESM SSS (PSU)", xpd = NA, lwd = 2,
+     cex.lab = 1.5, cex.axis = 1.5, main = "")
+# Modeled
+plot(time, coral, type = "l", frame.plot = F, ylab = "Pseudo-coral (modeled)",
+     xaxt = "n", xlab = "", xpd = NA, lwd = 2,
+     main = "", cex.lab = 1.5, cex.axis = 1.5)
+# Proxy
+plot(binned_proxy$x, binned_proxy$y, type = "l", frame.plot = F,
+     ylab = "Proxy data (permil)", xlab = "Year (CE)", xpd = NA,
+     lwd = 2, cex.lab = 1.5, cex.axis = 1.5)
+
+#### Plot pseudo vs. proxy correlation ####
+(cor = cor.test(coral, binned_proxy$y))
+plot(coral, binned_proxy$y, pch = 16, xlab = "Pseudo-coral",
+     ylab = "Proxy Data", main = c("r = 0.09", "p = 0.31"))
+
+#
+##############################################################################
 #
 #### Plot modeled SST and SSS data ####
 par(mfrow = c(2, 1), omi = c(0.7, 0.4, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
 plot(sst$x, sst_c, type = "l", frame.plot = F, xaxt = "n", xlab = "", lwd = 2,
-     ylab = "SST (C)", xpd = NA, main = "SWBB1 Lat:32.5 Lon:-64.7")
+     ylab = "SST (C)", xpd = NA, main = "KUBE1 Lat:32.5 Lon:-64.7")
 plot(sss$x, sss$y, type = "l", frame.plot = F, xlab = "Year (CE)",
      ylab = "SSS (PSU)", xpd = NA, lwd = 2)
 
@@ -65,15 +91,14 @@ plot(sss$x, sss$y, type = "l", frame.plot = F, xlab = "Year (CE)",
 #### Plot pseudo and binned proxy ####
 par(mfrow = c(2, 1), omi = c(0.7, 0.4, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
 # Pseudo
-plot(time, coral, type = "l", frame.plot = F, ylab = "SWBB2 Pseudo-coral",
+plot(time, coral, type = "l", frame.plot = F, ylab = "KUBE2 Pseudo-coral",
      xaxt = "n", xlab = "", xpd = NA, lwd = 2,
-     main = "SWBB1 Lat:32.5 Lon:-64.7")
+     main = "KUBE1 Lat:32.5 Lon:-64.7")
 # Proxy
 plot(binned_proxy$x, binned_proxy$y, type = "l", frame.plot = F,
      ylab = "Proxy data (permil)", xlab = "CE", xpd = NA,
      lwd = 2)
 
-###############################################################################
 #### Plot modeled and observed SST ####
 par(mfrow = c(2, 1), omi = c(0.7, 0.4, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
 plot(sst$x, sst_c, type = "l", frame.plot = F, xaxt = "n", xlab = "", lwd = 2,
@@ -109,58 +134,38 @@ plot(coral_obs, binned_proxy$y, pch = 16, xlab = "Observed SST pseudo-coral",
 
 #### Map proxy locations ####
 # Define archive point types
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+library(ggtext)
+data("wrld_simpl", package = "maptools")
+
 shapes = c("Coral" = 15, "GlacierIce" = 20, "Sclerosponge" = 19, "Wood" = 18,
            "LakeSediment" = 17, "Speleothem" = 17, "MolluskShells" = 19,
            "MarineSediment" = 8)
 
-coral_loc = matrix(NA, 1, 3) %>%
-        set_colnames(c("archive", "lat", "lon"))
-coral_loc[1] = bermuda$archiveType
-coral_loc[2] = bermuda$geo_latitude
-coral_loc[3] = bermuda$geo_longitude
-coral_loc = as.data.frame(coral_loc) %>%
-        mutate_at(c("lat", "lon"), as.numeric)
+proxy = c("KUBE" = 15, "KUBE" = 20, "KIPR" = 18)
+
+setwd("~/GitHub/iso-project/Iso2k_PRYSM_runs/data")
+coral_loc = read.csv("coral_proxy_locations.csv", head = T)
 
 ggplot() +
         geom_polygon(data = wrld_simpl, aes(x = long, y = lat, group = group), 
                      fill = "grey", colour = "black", alpha = 0.2) +
         geom_point(data = coral_loc, mapping = aes(x = lon, y = lat, 
-                                                    shape = archive), color = "black", size = 7) +        
-        geom_point(data = coral_loc, mapping = aes(x = lon, y = lat, 
-                                                    shape = archive, color = "blue"), size = 5) +
-        scale_shape_manual(values = shapes) +  
+                                                    shape = archive), color = "blue", size = 7) +
+        scale_shape_manual(values = proxy) + 
+        geom_textbox(data = coral_loc, aes(x = lon, y = lat, label = archive),
+                  hjust = -0.25, cex = 4, box.padding = unit(c(0,0,0,0), 'pt')) +
         # Removes Axes and labels
         scale_x_continuous(breaks = NULL) +
         scale_y_continuous(breaks = NULL) +
         xlab("") + 
         ylab("") +
+        coord_equal(xlim = c(-100, 20), ylim = c(0, 60)) +
         # Change theme to remove axes and ticks
         theme(panel.background = element_blank(),
               axis.ticks=element_blank(),
               panel.border = element_blank(),
               panel.grid.major = element_blank())
-
 #
-#### Full data stack ####
-par(mfrow = c(4, 1), omi = c(0.5, 0.3, 0, 0), mai = c(0.2, 0.5, 0.2, 0))
-# SST
-plot(sst$x, sst_c, type = "l", frame.plot = F, xaxt = "n", xlab = "", lwd = 2,
-     ylab = "CESM SST (C)", xpd = NA, main = "SWBB1 Lat:25.3903, Lon:-80.1715", 
-     cex.lab = 1.5, cex.axis = 1.5)
-# SSS
-plot(sss$x, sss$y, type = "l", frame.plot = F, xlab = "", xaxt = "n",
-     ylab = "CESM SSS (PSU)", xpd = NA, lwd = 2,
-     cex.lab = 1.5, cex.axis = 1.5, main = "")
-# Modeled
-plot(time, coral, type = "l", frame.plot = F, ylab = "Pseudo-coral (modeled)",
-     xaxt = "n", xlab = "", xpd = NA, lwd = 2,
-     main = "", cex.lab = 1.5, cex.axis = 1.5)
-# Proxy
-plot(binned_proxy$x, binned_proxy$y, type = "l", frame.plot = F,
-     ylab = "Proxy data (permil)", xlab = "Year (CE)", xpd = NA,
-     lwd = 2, cex.lab = 1.5, cex.axis = 1.5)
-
-#### Plot pseudo vs. proxy correlation ####
-(cor = cor.test(coral, binned_proxy$y))
-plot(coral, binned_proxy$y, pch = 16, xlab = "Pseudo-coral",
-     ylab = "Proxy Data", main = c("r = 0.09", "p = 0.31"))
