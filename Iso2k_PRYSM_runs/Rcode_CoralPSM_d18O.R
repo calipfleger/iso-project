b76f39a05f482d6063a97be47fc6b788e879c084
aflaim7 <aflaim@wustl.edu>
2021-11-19 16:18:09 -0600

    psm_runs_coral_1850-2005

---
+++ Iso2k_PRYSM_runs/Rcode_CoralPSM_d18O.R
@@ -0,0 +1,282 @@
+library(geoChronR)
+library(magrittr)
+library(tidyverse)
+library(lubridate)
+library(rgdal)
+library(ncdf4)
+library(ncdf4.helpers)
+
+#
+#### Import iCESM data ####
+setwd("D:/GitHub/iso-project/TS_1850")
+sst = read.csv("TS_full1_clipped.csv", head = T)
+setwd("D:/GitHub/iso-project/d18osw_coral")
+d18Osw = read.csv("test_full_post1.csv", head = T)
+
+# Trim to uniform timespan
+d18Osw = d18Osw[12:1872,]
+sst = sst[13:1873,]
+# Reformat time axis
+old.time = d18Osw[,1]
+d18Osw = d18Osw[,-1]
+sst = sst[,-1]
+start = 1851
+end = 2006
+monthly = seq(from = start, to = end, by = 1/12)
+
+# Remove incorrect records
+sst_trim = sst
+remove = vector()
+for (i in 1:ncol(sst)){
+  #print(i)
+  name = which(names(sst)[i] == names(d18Osw))
+  if (!length(name)){
+    remove = cbind(remove, i)}
+}
+sst_trim = sst[,-remove]
+sst = sst_trim
+
+# Convert sst to degrees C, not Kelvin.
+temp_flag = any(sst>200)
+for (i in 1:dim(sst)[2]){
+  for (j in 1:length(monthly)){
+    if (temp_flag){
+      sst[j, i] = sst[j, i]-274.15
+    }
+  }
+}
+
+# Convert to matching column order
+sst_rs = sst * 0
+for (i in 1:ncol(sst)){
+  col = which(names(sst) == names(d18Osw)[i])
+  sst_rs[,i] = sst[,col]
+}
+names(sst_rs) = names(d18Osw)
+sst = sst_rs
+
+#### PSM ####
+a = -0.22
+coral = matrix(data = NA, nrow = length(monthly), ncol = ncol(sst))
+for (i in 1:ncol(sst)){
+  for (j in 1:length(monthly)){
+    coral[j, i] = a*sst[j, i] + d18Osw[j, i]
+  }
+}
+coral = data.frame(coral)
+names(coral) = names(sst)
+
+coral.out = cbind(old.time, coral)
+names(coral.out)[1] = "time"
+#
+#### Export PSM output ####
+setwd("D:/GitHub/iso-project")
+write.csv(coral.out, "pseudocoral_1850-2005.csv", row.names = F)
+
+#
+#### Plot inputs and pseudocorals ####
+par(mfrow = c(3, 1), mai = c(0.5,0.5,0.5,0.5))
+plot(monthly, sst[,1], type = "l", xlab = "", xaxt = "n", ylab = "",
+     main = names(sst)[1], frame.plot = F)
+mtext(side = 2, line = 2.5, "SST (deg. C)")
+plot(monthly, d18Osw[,1], type = "l", xlab = "", xaxt = "n", ylab = "",
+     frame.plot = F)
+mtext(side = 2, line = 2.5, "d18Osw (permil)")
+plot(monthly, coral[,1], type = "l", xlab = "", ylab = "",
+     frame.plot = F)
+mtext(side = 2, line = 2.5, "Pseudocoral d18O")
+mtext(side = 1, line = 2, "Year (CE)")
+#
+
+# Stack all pseudocorals
+for (i in 1:ncol(coral)){
+  plot(monthly, coral[,i], type = "l", frame.plot = F, axes = F, xlab = "", 
+       ylab = "", ylim = range(coral))
+  par(new = T)
+}
+axis(1)
+mtext(side = 1, line = 2, "Year (CE)")
+axis(2)
+mtext(side = 2, line = 2.4, "Pseudocoral d18O")
+
+#
+#### Remove seasonal cycle ####
+require(graphics) 
+coralts = ts(coral[,1], start = 1850, end = 2005, deltat = 1/12) 
+stlcoral = stl(coralts, s.window = "periodic")
+plot(stlcoral)
+print(stlcoral$time.series)
+plot(stlcoral$time.series[,2]+stlcoral$time.series[,3])
+summary(stlcoral)
+#
+###############################################################################
+#### Plot Pseudocorals ####
+rec_col = c("blue", "red", "dark green", "purple", "black", "orange", "brown")
+for(i in 1:ncol(sst)){
+  plot(time, coral[,i], type = "l", frame.plot = F, ylim = c(-0.3,0.3),
+       xlab = "", ylab = "", axes = F, col = rec_col[i])
+  par(new = T)
+}
+plot(NA, frame.plot = F, ylim = c(-0.2,0.2), xlim = c(min(time), max(time)),
+     ylab = "Pseudo d18O anomalies", xlab = "Years (CE)", axes = T, main = "Pseudocorals")
+#### Plot Atlantic Output and PSMs ####
+# Plot SST
+for(i in 1:3){
+  plot(time, sst[,i], type = "l", frame.plot = F, ylim = c(-1,1),
+       xlab = "", ylab = "", axes = F, col = rec_col[i])
+  par(new = T)
+}
+plot(NA, frame.plot = F, ylim = c(-1,1), xlim = c(min(time), max(time)),
+     ylab = "SST anomalies", xlab = "Years (CE)", axes = T, main = "Atlantic iCESM full forcing SSTs")
+legend("topright", col = rec_col, 
+       legend = c("KUBE", "SWBB", "KIPR"),
+       lty = rep(1, 3))
+
+# Plot SSS
+for(i in 1:3){
+  plot(time, sss[,i], type = "l", frame.plot = F, ylim = c(-0.5,0.5),
+       xlab = "", ylab = "", axes = F, col = rec_col[i])
+  par(new = T)
+}
+plot(NA, frame.plot = F, ylim = c(-0.5,0.5), xlim = c(min(time), max(time)),
+     ylab = "SSS anomalies", xlab = "Years (CE)", axes = T, main = "Atlantic iCESM full forcing SSS")
+
+# Plot pseudoproxies
+for(i in 1:3){
+  plot(time, coral[,i], type = "l", frame.plot = F, ylim = c(-0.3,0.3),
+       xlab = "", ylab = "", axes = F, col = rec_col[i])
+  par(new = T)
+}
+plot(NA, frame.plot = F, ylim = c(-0.3,0.3), xlim = c(min(time), max(time)),
+     ylab = "Pseudo d18O anomalies", xlab = "Years (CE)", axes = T, main = "Atlantic Pseudocorals")
+
+# Plot proxies
+plot(time, KUBE_anom, type = "l", frame.plot = F, xlim = c(min(time), max(time)),
+     ylab = "", xlab = "", axes = T, main = "", col = rec_col[1],
+     ylim = c(-0.8, 0.8))
+par(new = T)
+plot(time, SWBB_anom, type = "l", frame.plot = F, xlim = c(min(time), max(time)),
+     ylab = "", xlab = "", axes = F, main = "", col = rec_col[2],
+     ylim = c(-0.8, 0.8))
+par(new = T)
+plot(time, KIPR_anom, type = "l", frame.plot = F, xlim = c(min(time), max(time)),
+     ylab = "d18O anomalies", xlab = "Years (CE)", axes = T, main = "Atlantic Iso2k coral anomalies",
+     col = rec_col[3], ylim = c(-0.8, 0.8))
+#
+#### Plot Pacific Output and PSMs ####
+# Plot SST
+for(i in 4:nrec){
+  plot(time, sst[,i], type = "l", frame.plot = F, ylim = c(-1,1),
+       xlab = "", ylab = "", axes = F, col = rec_col[i])
+  par(new = T)
+}
+plot(NA, frame.plot = F, ylim = c(-1,1), xlim = c(min(time), max(time)),
+     ylab = "SST anomalies", xlab = "Years (CE)", axes = T, main = "Pacific iCESM full forcing SSTs")
+legend("topright", col = rec_col[4:7], 
+       legend = c("LIRA", "NUMP", "COPM", "BOFP"),
+       lty = rep(1, 4))
+
+# Plot SSS
+for(i in 4:nrec){
+  plot(time, sss[,i], type = "l", frame.plot = F, ylim = c(-0.5,0.5),
+       xlab = "", ylab = "", axes = F, col = rec_col[i])
+  par(new = T)
+}
+plot(NA, frame.plot = F, ylim = c(-0.5,0.5), xlim = c(min(time), max(time)),
+     ylab = "SSS anomalies", xlab = "Years (CE)", axes = T, main = "Pacific iCESM full forcing SSS")
+
+# Plot pseudoproxies
+for(i in 4:nrec){
+  plot(time, coral[,i], type = "l", frame.plot = F, ylim = c(-0.3,0.3),
+       xlab = "", ylab = "", axes = F, col = rec_col[i])
+  par(new = T)
+}
+plot(NA, frame.plot = F, ylim = c(-0.3,0.3), xlim = c(min(time), max(time)),
+     ylab = "Pseudo d18O anomalies", xlab = "Years (CE)", axes = T, main = "Pacific Pseudocorals")
+
+# Plot proxies
+plot(time, LIRA_anom, type = "l", frame.plot = F, xlim = c(min(time), max(time)),
+     ylab = "", xlab = "", axes = T, main = "", col = rec_col[4],
+     ylim = c(-0.8, 0.8))
+par(new = T)
+plot(time, NUMP_anom, type = "l", frame.plot = F, xlim = c(min(time), max(time)),
+     ylab = "", xlab = "", axes = F, main = "", col = rec_col[5],
+     ylim = c(-0.8, 0.8))
+par(new = T)
+plot(time, COPM_anom, type = "l", frame.plot = F, xlim = c(min(time), max(time)),
+     ylab = "", xlab = "", axes = F, main = "", col = rec_col[6],
+     ylim = c(-0.8, 0.8))
+par(new = T)
+plot(time, BOFP_anom, type = "l", frame.plot = F, xlim = c(min(time), max(time)),
+     ylab = "d18O anomalies", xlab = "Years (CE)", axes = T, main = "Pacific Iso2k coral anomalies",
+     col = rec_col[7], ylim = c(-0.8, 0.8))
+#
+#### Extract observational SSTs ####
+setwd("C:/Users/andre/Box Sync/Konecky Lab/User Storage/Andrew Flaim/Rcode/Data") # Laptop
+
+data = nc_open("sst.mon.mean.nc")
+sst = ncvar_get(data, "sst")
+
+lon = ncvar_get(data, varid = "lon")
+lat = ncvar_get(data, varid = "lat")
+sst_time = nc.get.time.series(data, v = "sst",
+                              time.dim.name = "time")
+# Extract grided data
+time_index = which(format(sst_time, "%Y-%m-%d") == "1980-12-01")
+sst.slice = sst[,,time_index]
+
+bofp = sst[210, -17.5, ]
+
+sst.location = bofp
+trim = 1:1573
+trim_time = sst_time[trim]
+
+test = tibble(time = trim_time, 
+              sst = as.vector(sst.location[trim])) %>% 
+  mutate(time = as.Date(format(time, "%Y-%m-%d")))
+
+# Bin timeseries
+binvec =  seq(from = 1850, to = 1980, by = 1)
+year = seq(from = 1850, to = 1981, by = 1/12)
+thisrec = na.omit(data.frame(year = year,
+                             val = test$sst))
+binned_ann_recs = geoChronR::bin(time = thisrec$year, 
+                                 val = thisrec$val, bin.vec = binvec)
+sst_obs = binned_ann_recs$y-mean(na.omit(binned_ann_recs$y))
+
+nc_close("sst.mon.mean.nc")
+#
+#### BOFP Moorea, French Polynesia - Fixed d18O with observational temp ####
+# d18Osw = 0.6 from Conroy 2017
+d18O = 0.6
+a = -0.22
+fixed = matrix(data = NA, nrow = length(time), ncol = 1)
+
+for (i in 1:length(time)){
+  fixed[i,] = a*sst_obs[i] + d18O
+}
+
+# Fixed with modeled temp
+fixed_mod = matrix(data = NA, nrow = length(time), ncol = 1)
+for (i in 1:length(time)){
+  fixed_mod[i,] = a*sst[i,7] + d18O
+}
+#### Plot fixed obs. input and output ####
+par(mfrow = c(4,1), mai = c(0, 0, 0, 0), omi = c(0.75, 0.75, 0.25, 0))
+plot(time, sst[,7], type = "l", frame.plot = F, xaxt = "n", xlab = "", xpd = NA,
+     ylab = "iCESM SST", cex.lab = 1.5, cex.axis = 1.5)
+plot(time, fixed_mod, type = "l", frame.plot = F, xaxt = "n", xlab = "", xpd = NA,
+     ylab = "iCESM Pseudo d18O", cex.lab = 1.5, cex.axis = 1.5)
+plot(time, sst_obs, type = "l", frame.plot = F, xaxt = "n", xlab = "", xpd = NA,
+     ylab = "COBE-SST", cex.lab = 1.5, cex.axis = 1.5)
+plot(time, fixed, type = "l", frame.plot = F, xlab = "Years (CE)", xpd = NA,
+     ylab = "Obs. Pseudo d18O", cex.lab = 1.5, cex.axis = 1.5)
+
+# Fixed model, obs, and proxy
+par(mfrow = c(3,1), mai = c(0, 0, 0, 0), omi = c(0.75, 0.75, 0.25, 0))
+plot(time, BOFP_anom, type = "l", frame.plot = F, xaxt = "n", xlab = "", xpd = NA,
+     ylab = "Iso2k d18O", cex.lab = 1.5, cex.axis = 1.5)
+plot(time, fixed_mod, type = "l", frame.plot = F, xaxt = "n", xlab = "", xpd = NA,
+     ylab = "iCESM Pseudo d18O", cex.lab = 1.5, cex.axis = 1.5)
+plot(time, fixed, type = "l", frame.plot = F, xlab = "Years (CE)", xpd = NA,
+     ylab = "Obs. Pseudo d18O", cex.lab = 1.5, cex.axis = 1.5)
